551c1e846784dbdcbba72baa54aa58f7bbee7d57
brauliomendez <braauliomendez@ciencias.unam.mx>
2017-05-19 09:14:08 -0500

    NEW: Initial commit.

---
+++ app/src/main/java/com/brauliomendez/androidarchitecture/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.brauliomendez.androidarchitecture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
